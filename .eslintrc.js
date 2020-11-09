e01433e04c9e4a2e8a5e3c92bdb40b847c5d7bbd
unknown <acolpron@turbulent.ca>
2020-11-09 12:35:17 -0500

    settings files

---
+++ .eslintrc.js
@@ -0,0 +1,76 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb',
+    // 'plugin:@typescript-eslint/recommended',
+    'prettier',
+    // 'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  // parser: '@typescript-eslint/parser',
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['import', 'prettier', 'react'],
+  // plugins: ['import', 'prettier', '@typescript-eslint', 'react', 'react-hooks'],
+  rules: {
+    // prettier
+    'prettier/prettier': ['error'],
+
+    // import
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        mjs: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-alert': 'off',
+
+    // '@typescript-eslint/explicit-function-return-type': 'off',
+    // '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
+    // '@typescript-eslint/explicit-module-boundary-types': 'off',
+    // '@typescript-eslint/no-explicit-any': 'off',
+    // '@typescript-eslint/no-var-requires': 'off',
+    // '@typescript-eslint/no-unused-vars': 'error',
+
+    'react/destructuring-assignment': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', 'tsx'] }],
+  },
+  overrides: [
+    {
+      // enable the rule specifically for TypeScript files
+      files: ['*.ts'],
+      rules: {
+        // '@typescript-eslint/explicit-function-return-type': ['error'],
+      },
+    },
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+      },
+    },
+    'import/extensions': ['.js', '.ts', '.mjs', '.jsx', '.tsx'],
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+// Commented lines is the default config from prettier
+module.exports = {
+  printWidth: 120,
+  // tabWidth: 2,
+  // useTabs: false,
+  // semi: true,
+  singleQuote: true,
+  // quoteProps: 'as-needed',
+  // jsxSingleQuote: false,
+  trailingComma: 'all',
+  endOfLine: 'auto',
+  // bracketSpacing: true,
+  // jsxBracketSameLine: false,
+  // arrowParens: 'always',
+};
