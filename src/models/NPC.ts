826aff04aee1599a9c2b916532c2471db416ddde
unknown <acolpron@turbulent.ca>
2020-11-11 12:10:45 -0500

    last fixes for store and model

--- src/models/NPC.ts
+++ src/models/NPC.ts
@@ -1,3 +1,4 @@
+import { copyObject } from '../Tools/functions';
 import { NPCStatuses } from './enums';
 import { defaultTraits, Aptitudes } from './interfaces';
 
@@ -43,10 +44,6 @@ class NPC {
       if (data.name !== undefined) this.name = data.name;
       if (data.size !== undefined) this.size = data.size;
       if (data.pace !== undefined) this.pace = data.pace;
-      if (data.woundPenalties !== undefined) this.woundPenalties = data.woundPenalties;
-      if (data.otherModifiers !== undefined) this.otherModifiers = data.otherModifiers;
-      if (data.status !== undefined) this.status = data.status;
-      if (data.stun !== undefined) this.stun = data.stun;
       if (data.note !== undefined) this.note = data.note;
       if (data.wind !== undefined) this.wind = data.wind;
       if (data.undead !== undefined) this.undead = data.undead;
@@ -55,9 +52,9 @@ class NPC {
       if (data.thickSkinned !== undefined) this.thickSkinned = data.thickSkinned;
       if (data.sand !== undefined) this.sand = data.sand;
       if (data.woundsToKill !== undefined) this.woundsToKill = data.woundsToKill;
-      if (data.traits !== undefined) this.traits = JSON.parse(JSON.stringify(data.traits));
-      if (data.aptitudes !== undefined) this.aptitudes = JSON.parse(JSON.stringify(data.aptitudes));
-      if (data.attacks !== undefined) this.attacks = JSON.parse(JSON.stringify(data.attacks));
+      if (data.traits !== undefined) this.traits = copyObject(data.traits);
+      if (data.aptitudes !== undefined) this.aptitudes = copyObject(data.aptitudes);
+      if (data.attacks !== undefined) this.attacks = copyObject(data.attacks);
     }
   }
 }
