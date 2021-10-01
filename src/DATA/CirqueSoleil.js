4b10fb81227f07ed2a5db8bbcd6c82862ae160bf
COMPU-DOGE\Anton <anthonycolpron@gmail.com>
2021-09-30 20:00:25 -0400

    added scrap glom

--- src/DATA/CirqueSoleil.js
+++ src/DATA/CirqueSoleil.js
@@ -52,6 +52,58 @@ const cirqueSoleil = {
       ],
       note: 'Armor: 1. If raise: Acidic Bite',
     },
+    {
+      name: 'Scrap Glom',
+      pace: 6,
+      size: 16,
+      wind: 120,
+      undead: true,
+      traits: {
+        deftness: { level: 2, diceType: 6 },
+        nimbleness: { level: 2, diceType: 6 },
+        strength: { level: 2, diceType: 12, dicePlus: 4 },
+        quickness: { level: 3, diceType: 8 },
+        vigor: { level: 2, diceType: 12, dicePlus: 4 },
+        cognition: { level: 2, diceType: 10 },
+        knowledge: { level: 2, diceType: 4 },
+        mien: { level: 1, diceType: 12, dicePlus: 2 },
+        smarts: { level: 1, diceType: 6 },
+        spirit: { level: 1, diceType: 8 },
+      },
+      aptitudes: {
+        corporeal: {
+          climbin: {
+            level: 5,
+          },
+          fightin: {
+            level: 6,
+            concentrations: [`brawlin'`],
+          },
+          shootin: {
+            level: 3,
+            concentrations: ['acid'],
+          },
+          sneak: {
+            level: 3,
+          },
+        },
+        mental: {
+          search: {
+            level: 3,
+          },
+          overawe: {
+            level: 5,
+          },
+        },
+      },
+      attacks: [
+        { name: 'Bodies slam', stats: { numberOfDices: 0, diceType: 4, addStrength: true } },
+        { name: 'Gatling Gun x 2', stats: { numberOfDices: 3, diceType: 8 } },
+        { name: 'Acid Spray', stats: { numberOfDices: 4, diceType: 8 } },
+        { name: 'FlameThrower', stats: { numberOfDices: 4, diceType: 10 } },
+      ],
+      note: 'Armor: 2',
+    },
   ],
 };
 
